0003328de18dfca9465beb46e6cd43409e7eae31
bbx1209 <0weibo0@163.com>
2021-08-01 10:11:30 +0800

    list item

--- Android/app/src/main/java/com/helloworld/ListViewActivity.java
+++ Android/app/src/main/java/com/helloworld/ListViewActivity.java
@@ -3,6 +3,10 @@ package com.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,6 +25,17 @@ public class ListViewActivity extends AppCompatActivity {
             item.setName("item" + i);
             data.add(item);
         }
+
+        ListView listView = findViewById(R.id.LV);
+        listView.setAdapter(new MyAdeptor(data, this));
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.e(" item ", "onItemClick: " + position );
+            }
+        });
+
 
     }
 }
