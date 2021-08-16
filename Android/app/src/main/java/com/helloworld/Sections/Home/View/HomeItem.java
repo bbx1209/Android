f0a79a9b0e252e6fcb7be68aebb049d78d2ed936
bbx1209 <0weibo0@163.com>
2021-08-16 18:01:42 +0800

    home list

--- Android/app/src/main/java/com/helloworld/Sections/Home/View/HomeItem.java
+++ Android/app/src/main/java/com/helloworld/Sections/Home/View/HomeItem.java
@@ -25,10 +25,10 @@ public class HomeItem extends Fragment {
 
     }
 
-//    @Override
-//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-//                             Bundle savedInstanceState) {
-//        // Inflate the layout for this fragment
-//        return inflater.inflate(R.layout.fragment_home_item, container, false);
-//    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home_item, container, false);
+    }
 }
