0d3f97b9f3d0d6036ebc0cf6dd37774d0d9c78a0
bbx1209 <0weibo0@163.com>
2021-08-19 22:28:11 +0800

    pass vaule activity

---
+++ Android/app/src/main/java/com/helloworld/Sections/PassParam/PassParam.java
@@ -0,0 +1,17 @@
+package com.helloworld.Sections.PassParam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.helloworld.R;
+
+
+public class PassParam extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pass_param);
+    }
+}
