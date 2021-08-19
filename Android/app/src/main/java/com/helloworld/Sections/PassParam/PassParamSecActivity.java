0dd793c0932b361c70009cc256dfb7e6ae2b2403
bbx1209 <0weibo0@163.com>
2021-08-19 22:31:38 +0800

    second activity

---
+++ Android/app/src/main/java/com/helloworld/Sections/PassParam/PassParamSecActivity.java
@@ -0,0 +1,16 @@
+package com.helloworld.Sections.PassParam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.helloworld.R;
+
+public class PassParamSecActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pass_param_sec);
+    }
+}
