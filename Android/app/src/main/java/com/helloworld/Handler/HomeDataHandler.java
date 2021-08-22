0d018408bde970ca1cc1478605acc250398c589d
bbx1209 <0weibo0@163.com>
2021-08-22 10:58:37 +0800

    UI activity

--- Android/app/src/main/java/com/helloworld/Handler/HomeDataHandler.java
+++ Android/app/src/main/java/com/helloworld/Handler/HomeDataHandler.java
@@ -4,6 +4,7 @@ import com.helloworld.BaseModel.ActivityModel;
 import com.helloworld.Sections.NoficaitonActiviry.Notifaction;
 import com.helloworld.Sections.ListView.ListViewActivity;
 import com.helloworld.Sections.PassParam.PassParam;
+import com.helloworld.Sections.UIElementActivity.UIElementActivity;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,6 +20,8 @@ public class HomeDataHandler {
         list.add(model2);
         ActivityModel model3 = new ActivityModel(PassParam.class, "页面传值", null);
         list.add(model3);
+        ActivityModel model4 = new ActivityModel(UIElementActivity.class, "UI元素", null);
+        list.add(model4);
 
         return list;
     }
