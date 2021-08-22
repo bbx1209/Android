e3d34d94733957832122dbf96b8367f2b859d9cb
bbx1209 <0weibo0@163.com>
2021-08-22 10:55:14 +0800

    UIelement activity

---
+++ Android/app/src/main/java/com/helloworld/Sections/UIElementActivity/UIElementActivity.java
@@ -0,0 +1,16 @@
+package com.helloworld.Sections.UIElementActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.helloworld.R;
+
+public class UIElementActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_uielement);
+    }
+}
