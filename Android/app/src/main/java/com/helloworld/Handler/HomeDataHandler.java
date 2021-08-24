f38bfe0b5e552c48eed0ae4f2d0a21b78ffc985b
bbx1209 <0weibo0@163.com>
2021-08-24 18:54:38 +0800

    pop window

--- Android/app/src/main/java/com/helloworld/Handler/HomeDataHandler.java
+++ Android/app/src/main/java/com/helloworld/Handler/HomeDataHandler.java
@@ -4,6 +4,7 @@ import com.helloworld.BaseModel.ActivityModel;
 import com.helloworld.Sections.NoficaitonActiviry.Notifaction;
 import com.helloworld.Sections.ListView.ListViewActivity;
 import com.helloworld.Sections.PassParam.PassParam;
+import com.helloworld.Sections.Popwindow.PopWindowActivity;
 import com.helloworld.Sections.UIElementActivity.UIElementActivity;
 
 import java.util.ArrayList;
@@ -22,6 +23,8 @@ public class HomeDataHandler {
         list.add(model3);
         ActivityModel model4 = new ActivityModel(UIElementActivity.class, "UI元素", null);
         list.add(model4);
+        ActivityModel model5 = new ActivityModel(PopWindowActivity.class, "PopWindow", null);
+        list.add(model5);
 
         return list;
     }
---
+++ Android/app/src/main/java/com/helloworld/Sections/Popwindow/PopWindowActivity.java
@@ -0,0 +1,56 @@
+package com.helloworld.Sections.Popwindow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.PopupWindow;
+
+import com.helloworld.R;
+
+
+public class PopWindowActivity extends AppCompatActivity {
+
+    private static final String TAG = "bbx";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pop_window);
+    }
+
+    public void showPopWindow(View view) {
+
+        View contentView = getLayoutInflater().inflate(R.layout.pop_content_viiew, null);
+
+        Button button1 = contentView.findViewById(R.id.but1);
+        Button button2 = contentView.findViewById(R.id.but2);
+
+
+        PopupWindow popupWindow = new PopupWindow(contentView, ViewGroup.LayoutParams.MATCH_PARENT, 300, true);
+
+        popupWindow.setBackgroundDrawable(getResources().getDrawable(R.drawable.image));
+
+
+        popupWindow.showAsDropDown(view);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.e(TAG, "onClick:第一个按钮");
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.e(TAG, "onClick:第二个按钮");
+            }
+        });
+
+
+    }
+}
