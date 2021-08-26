afac2d3cebb2fba9a8f6e49b0d8623d2750338d7
bbx1209 <0weibo0@163.com>
2021-08-26 14:37:27 +0800

    补间动画

--- Android/app/src/main/java/com/helloworld/Sections/Animation/AnimationActivity.java
+++ Android/app/src/main/java/com/helloworld/Sections/Animation/AnimationActivity.java
@@ -6,6 +6,8 @@ import android.graphics.drawable.AnimationDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 import com.helloworld.R;
@@ -14,6 +16,8 @@ import com.helloworld.R;
 public class AnimationActivity extends AppCompatActivity {
 
     private   AnimationDrawable drawable;
+    private   ImageView _imageView;
+    private  boolean animating = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,11 +28,13 @@ public class AnimationActivity extends AppCompatActivity {
         imageView.setImageResource(R.drawable.walk_animate);
         drawable = (AnimationDrawable) imageView.getDrawable();
 
+
+
+        _imageView = findViewById(R.id.alpha_image);
+
     }
 
     public void stopAnimationList(View view) {
-
-
         drawable.stop();
     }
 
@@ -37,14 +43,38 @@ public class AnimationActivity extends AppCompatActivity {
     }
 
     public void startAlpha(View view) {
+
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.alpha);
+        startAnimation(animation);
+
     }
 
     public void startRodate(View view) {
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.rotate);
+        startAnimation(animation);
     }
 
+
     public void startTranslate(View view) {
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.translate);
+        startAnimation(animation);
     }
 
     public void startScale(View view) {
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.scale);
+        startAnimation(animation);
+
     }
+
+    public void startAnimation(Animation aAnimation) {
+        Animation oldAnimaiton = _imageView.getAnimation();
+        if (oldAnimaiton == null) {
+            _imageView.startAnimation(aAnimation);
+        } else {
+            _imageView.clearAnimation();
+            _imageView.startAnimation(aAnimation);
+        }
+
+    }
+
 }
