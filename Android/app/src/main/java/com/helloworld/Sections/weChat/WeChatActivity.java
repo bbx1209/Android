338aa7331007d36eef00bdf3f436fc7d272d94a0
bbx1209 <0weibo0@163.com>
2021-08-29 08:09:20 +0800

    wechat bottom bar layout and drable

---
+++ Android/app/src/main/java/com/helloworld/Sections/weChat/WeChatActivity.java
@@ -0,0 +1,17 @@
+package com.helloworld.Sections.weChat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.helloworld.R;
+
+
+public class WeChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_we_chat);
+    }
+}
