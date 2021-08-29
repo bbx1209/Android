baea4a79b6ea9d1e4342b966625c29b121c391a9
bbx1209 <0weibo0@163.com>
2021-08-29 09:21:38 +0800

    微信架构联动

--- Android/app/src/main/java/com/helloworld/Sections/weChat/WeChatActivity.java
+++ Android/app/src/main/java/com/helloworld/Sections/weChat/WeChatActivity.java
@@ -8,6 +8,8 @@ import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.helloworld.R;
 
@@ -15,15 +17,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class WeChatActivity extends AppCompatActivity {
+public class WeChatActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private LinearLayout llchat, llcontact, llfaxian, llwo;
+    private ImageView ivchat, ivcontact, ivfaxian, ivwo, current;
+    private  ViewPager2 viewpager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_we_chat);
 
+        initSubViews();
 
-        ViewPager2 viewpager = findViewById(R.id.viewpager);
+
+         viewpager = findViewById(R.id.viewpager);
 
         List<Fragment> fragments = new ArrayList<>();
 
@@ -35,5 +43,81 @@ public class WeChatActivity extends AppCompatActivity {
         MyFragmentAdapter myFragmentAdapter = new MyFragmentAdapter(getSupportFragmentManager(), getLifecycle(), fragments);
 
         viewpager.setAdapter(myFragmentAdapter);
+
+        viewpager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                super.onPageScrolled(position, positionOffset, positionOffsetPixels);
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                changeTap(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                super.onPageScrollStateChanged(state);
+            }
+        });
+    }
+
+    private void changeTap(int position) {
+        current.setSelected(false);
+        switch (position) {
+            case R.id.chat_item_layout:
+                viewpager.setCurrentItem(0);
+            case 0:
+                ivchat.setSelected(true);
+                current = ivchat;
+                break;
+            case R.id.contact_item_layout:
+                viewpager.setCurrentItem(1);
+            case 1:
+                ivcontact.setSelected(true);
+                current = ivcontact;
+                break;
+            case R.id.faxian_item_layout:
+                viewpager.setCurrentItem(2);
+            case 2:
+                ivfaxian.setSelected(true);
+                current = ivfaxian;
+                break;
+            case R.id.wo_item_layout:
+                viewpager.setCurrentItem(3);
+            case 3:
+                ivwo.setSelected(true);
+                current = ivwo;
+                break;
+
+        }
+    }
+
+
+    private void initSubViews() {
+
+        llchat = findViewById(R.id.chat_item_layout);
+        llchat.setOnClickListener(this);
+        llcontact = findViewById(R.id.contact_item_layout);
+        llcontact.setOnClickListener(this);
+        llfaxian = findViewById(R.id.faxian_item_layout);
+        llfaxian.setOnClickListener(this);
+        llwo = findViewById(R.id.wo_item_layout);
+        llwo.setOnClickListener(this);
+
+        ivchat = findViewById(R.id.chat_iv);
+        ivcontact = findViewById(R.id.contact_iv);
+        ivfaxian = findViewById(R.id.faxian_iv);
+        ivwo = findViewById(R.id.wo_iv);
+
+        ivchat.setSelected(true);
+        current = ivchat;
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        changeTap(v.getId());
     }
 }
