04279fa71a4e305f8cd791ea07f3d61ec69e76f5
bbx1209 <0weibo0@163.com>
2021-09-01 20:50:08 +0800

    write in  and read from local file

--- Android/app/src/main/java/com/helloworld/Sections/database/DataBaseActivity.java
+++ Android/app/src/main/java/com/helloworld/Sections/database/DataBaseActivity.java
@@ -2,11 +2,21 @@ package com.helloworld.Sections.database;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
 import com.helloworld.R;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class DataBaseActivity extends AppCompatActivity {
 
@@ -29,10 +39,64 @@ public class DataBaseActivity extends AppCompatActivity {
 
     // 写入文件
     public void writeInfile(View view) {
+
+        String data = "this is the data to store";
+        FileOutputStream out = null;
+        BufferedWriter writer = null;
+
+        try {
+            out = openFileOutput("data", Context.MODE_PRIVATE);
+            writer = new BufferedWriter(new OutputStreamWriter(out));
+            writer.write(data);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (writer != null) {
+                    writer.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+
     }
 
     //从文件中读取数据
     public void readFromfile(View view) {
+        FileInputStream in = null;
+        BufferedReader reader = null;
+        StringBuilder content = new StringBuilder();
+
+        try {
+            in = openFileInput("data");
+            reader = new BufferedReader(new InputStreamReader(in));
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                content.append(line);
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+           if (reader != null){
+               try {
+                   reader.close();
+               } catch (IOException e) {
+                   e.printStackTrace();
+               }
+           }
+        }
+
+        String result = content.toString();
+        fileEditText.setText(result);
     }
 
     // 写入SP
