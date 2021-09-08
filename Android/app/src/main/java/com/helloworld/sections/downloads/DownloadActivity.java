5511bc91dc621207617e35701c11bd21f5c31016
bbx1209 <0weibo0@163.com>
2021-09-08 18:10:24 +0800

    down load

--- Android/app/src/main/java/com/helloworld/sections/downloads/DownloadActivity.java
+++ Android/app/src/main/java/com/helloworld/sections/downloads/DownloadActivity.java
@@ -6,15 +6,32 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.view.View;
 
 import com.helloworld.R;
 import com.helloworld.Sections.downloads.DownloadService;
+import com.helloworld.Sections.downloads.DownloadService.DownloadBinder;
 
 public class DownloadActivity extends AppCompatActivity {
+
+    private DownloadService.DownloadBinder downloadBinder;
+    private ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            downloadBinder = (DownloadBinder) service;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,28 +40,36 @@ public class DownloadActivity extends AppCompatActivity {
 
         Intent intent = new Intent(this, DownloadService.class);
         startService(intent);
+        bindService(intent, connection, BIND_AUTO_CREATE);
 
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new  String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
         }
 
     }
 
     public void startLoad(View view) {
-
-
+        if (downloadBinder == null) {
+            return;
+        }
+        String url = "https://raw.githubusercontent.com/guolindev/eclipse/master/eclipse-int-win64.exe";
+        downloadBinder.startDownload(url);
 
     }
 
     public void pauseLoad(View view) {
-
+        if (downloadBinder == null) {
+            return;
+        }
+        downloadBinder.pauseDownload();
 
     }
 
     public void cancelLoad(View view) {
-
-
-
+        if (downloadBinder == null) {
+            return;
+        }
+        downloadBinder.cancelDownload();
     }
 
 
@@ -56,5 +81,12 @@ public class DownloadActivity extends AppCompatActivity {
                 break;
             default:
         }
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unbindService(connection);
     }
 }
--- Android/app/src/main/java/com/helloworld/sections/downloads/DownloadService.java
+++ Android/app/src/main/java/com/helloworld/sections/downloads/DownloadService.java
@@ -1,11 +1,14 @@
 package com.helloworld.Sections.downloads;
 
 import android.app.Notification;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.os.Binder;
+import android.os.Environment;
 import android.os.IBinder;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.core.app.NotificationCompat;
@@ -14,36 +17,50 @@ import com.helloworld.Sections.downloads.DownloadListener;
 import com.helloworld.Sections.downloads.DownloadTask;
 import com.helloworld.sections.downloads.DownloadActivity;
 
+import java.io.File;
 
-public class DownloadService  extends Service {
+
+public class DownloadService extends Service {
 
     private DownloadTask downloadTask;
-    private  String downloadUrl;
+    private String downloadUrl;
 
     private DownloadListener listener = new DownloadListener() {
         @Override
         public void onProgress(int progress) {
-//            getNotificationManager()
+            getNotificationManager().notify(1, getNotification("Downloading ... ", progress));
         }
 
 
         @Override
         public void onSuccess() {
-
+        downloadTask= null;
+        stopForeground(true);
+        getNotificationManager().notify(1, getNotification("Download success", -1));
+            Toast.makeText(DownloadService.this, "Download Success", Toast.LENGTH_SHORT).show();
         }
 
         @Override
         public void onFailed() {
+            downloadTask = null;
+            stopForeground(true);
+            getNotificationManager().notify(1, getNotification("Download Faild", -1));
+            Toast.makeText(DownloadService.this, "Download Faild", Toast.LENGTH_SHORT).show();
 
         }
 
         @Override
         public void onPause() {
+            downloadTask = null;
+            Toast.makeText(DownloadService.this, "Download Paused", Toast.LENGTH_SHORT).show();
 
         }
 
         @Override
         public void onCancel() {
+            downloadTask = null;
+            stopForeground(true);
+            Toast.makeText(DownloadService.this, "Download Canceld", Toast.LENGTH_SHORT).show();
 
         }
     };
@@ -54,19 +71,64 @@ public class DownloadService  extends Service {
         return mBinder;
     }
 
-    private  DownloadBinder  mBinder = new DownloadBinder();
+    private DownloadBinder mBinder = new DownloadBinder();
 
-    class  DownloadBinder extends Binder {
+    public class DownloadBinder extends Binder {
 
+        public  void startDownload(String url) {
+            if (downloadTask == null) {
+                downloadUrl = url;
+                downloadTask = new DownloadTask(listener);
+                downloadTask.execute(downloadUrl);
+                startForeground(1, getNotification("Downloading ...", 0));
+                Toast.makeText(DownloadService.this, "Downloading ...", Toast.LENGTH_SHORT).show();
+
+            }
+        }
+        public  void  pauseDownload(){
+            if (downloadTask != null) {
+                downloadTask.pauseDownload();
+            }
+        }
+        public void cancelDownload() {
+            if (downloadTask != null){
+                downloadTask.cancelDownload();
+            }
+            if (downloadUrl != null) {
+                //取消下载是需要将文件删除,并通知关闭
+                String fileName = downloadUrl.substring(downloadUrl.lastIndexOf("/"));
+                String directory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getPath();
+                File file = new File(directory + fileName);
+                if (file.exists()) {
+                    file.delete();
+                }
+                getNotificationManager().cancel(1);
+                stopForeground(true);
+                Toast.makeText(DownloadService.this, "Canceld", Toast.LENGTH_SHORT).show();
+
+
+            }
+        }
 
 
     }
 
-    private Notification getNotificationManager(String title, int progress) {
+    private Notification getNotification(String title, int progress) {
 
         Intent intent = new Intent(this, DownloadActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+        builder.setSmallIcon(android.R.mipmap.sym_def_app_icon);
+        builder.setContentIntent(pendingIntent);
+        builder.setContentTitle(title);
+        if (progress >= 0) {
+            builder.setContentText(progress +  "%");
+            builder.setProgress(100, progress, false);
+        }
+
         return builder.build();
+    }
+    private NotificationManager getNotificationManager() {
+        return (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
     }
 }
