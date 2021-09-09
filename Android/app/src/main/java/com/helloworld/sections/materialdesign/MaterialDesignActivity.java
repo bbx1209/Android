a0c6f2e8fdcde5e9717b1a447e5e8f8e4ca9d1c4
bbx1209 <0weibo0@163.com>
2021-09-09 14:45:14 +0800

    drawer click lestner

--- Android/app/src/main/java/com/helloworld/sections/materialdesign/MaterialDesignActivity.java
+++ Android/app/src/main/java/com/helloworld/sections/materialdesign/MaterialDesignActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -14,6 +15,7 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.google.android.material.navigation.NavigationView;
 import com.helloworld.R;
 
 public class MaterialDesignActivity extends AppCompatActivity {
@@ -32,6 +34,35 @@ public class MaterialDesignActivity extends AppCompatActivity {
             actionBar.setDisplayHomeAsUpEnabled(true);
             actionBar.setHomeAsUpIndicator(R.drawable.menu);
         }
+
+        NavigationView navi = (NavigationView) findViewById(R.id.navi_view);
+        navi.setCheckedItem(R.id.call);
+        navi.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @SuppressLint("NonConstantResourceId")
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.call:
+                        Toast.makeText(MaterialDesignActivity.this, "call", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.friends:
+                        Toast.makeText(MaterialDesignActivity.this, "friends", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.mail:
+                        Toast.makeText(MaterialDesignActivity.this, "mail", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.task:
+                        Toast.makeText(MaterialDesignActivity.this, "task", Toast.LENGTH_SHORT).show();
+                        break;
+                    default:
+                }
+
+                //关闭drawer
+                drawerLayout.closeDrawers();
+                return false;// boolean 标记item 是不是显示选中状态
+            }
+        });
+
     }
 
 
