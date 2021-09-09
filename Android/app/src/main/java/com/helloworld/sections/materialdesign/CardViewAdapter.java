012b62dc6bad8cb1eb76765641f46dd4e1c2be31
bbx1209 <0weibo0@163.com>
2021-09-09 21:21:26 +0800

    可折叠的标题栏

--- Android/app/src/main/java/com/helloworld/sections/materialdesign/CardViewAdapter.java
+++ Android/app/src/main/java/com/helloworld/sections/materialdesign/CardViewAdapter.java
@@ -1,6 +1,7 @@
 package com.helloworld.Sections.materialdesign;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +15,8 @@ import com.bumptech.glide.Glide;
 import com.helloworld.R;
 
 import java.util.List;
+import com.helloworld.Sections.materialdesign.CardModel;
+import com.helloworld.sections.materialdesign.CardDetailActivity;
 
 public class CardViewAdapter extends RecyclerView.Adapter<CardViewAdapter.ViewHolder> {
 
@@ -32,8 +35,21 @@ public class CardViewAdapter extends RecyclerView.Adapter<CardViewAdapter.ViewHo
             context = parent.getContext();
         }
         View view = LayoutInflater.from(context).inflate(R.layout.card_view_layout, parent, false);
+        ViewHolder viewHolder = new ViewHolder(view);
 
-        return new ViewHolder(view);
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int position = viewHolder.getAdapterPosition();
+                CardModel model = data.get(position);
+                Intent intent = new Intent(context, CardDetailActivity.class);
+                intent.putExtra("name", model.getName());
+                intent.putExtra("imageId", model.getImageId());
+                context.startActivity(intent);
+            }
+        });
+
+        return viewHolder;
     }
 
     @Override
