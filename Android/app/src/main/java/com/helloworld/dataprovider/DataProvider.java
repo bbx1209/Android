5a17700a0cea5bfb755ad11280b51763eca3f912
bbx1209 <0weibo0@163.com>
2021-09-11 16:58:05 +0800

    audio recoder activity

---
+++ Android/app/src/main/java/com/helloworld/dataprovider/DataProvider.java
@@ -0,0 +1,16 @@
+package com.helloworld.dataprovider;
+
+import java.util.ArrayList;
+import java.util.List;
+import com.helloworld.dataprovider.DataItem;
+
+public class DataProvider {
+
+
+    public  static List<DataItem> getDataItems() {
+        List<DataItem> datas = new ArrayList<>();
+
+        return datas;
+    }
+
+}
