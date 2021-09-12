549522cd87eef230c4b19e03bb7813e80b72fcc0
bbx1209 <0weibo0@163.com>
2021-09-12 11:17:30 +0800

    异步线程

--- Android/app/src/main/java/com/helloworld/sections/thread/ThreadActivity.java
+++ Android/app/src/main/java/com/helloworld/sections/thread/ThreadActivity.java
@@ -10,6 +10,12 @@ import com.helloworld.R;
 import com.helloworld.sections.thread.MyThread;
 import com.helloworld.sections.thread.MyRunable;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 public class ThreadActivity extends AppCompatActivity {
 
     private static final String TAG = "=== ThreadActivity ===";
@@ -41,5 +47,35 @@ public class ThreadActivity extends AppCompatActivity {
 
         thread.start();
 
+    }
+
+    public void onClickCallable(View view) {
+        MyCallAble myCallAble = new MyCallAble();
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        Future submit = executorService.submit(myCallAble);
+
+        String retValue = null;
+        try {
+            retValue = (String) submit.get();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Log.d(TAG, "onClickCallable: " + retValue + Thread.currentThread().toString());
+
+    }
+    //开启一个一步线程   ->begin->end->run
+    public void startAnewThread(View view) {
+
+        Log.d(TAG, "startAnewThread: " + "begin" + Thread.currentThread().toString());
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Log.d(TAG, "onClickCallable: " + "run" + Thread.currentThread().toString());
+            }
+        }).start();
+        Log.d(TAG, "onClickCallable: " + "end" + Thread.currentThread().toString());
     }
 }
