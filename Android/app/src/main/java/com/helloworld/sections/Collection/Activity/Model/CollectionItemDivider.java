64fbee8cf9819afa29f9257f7722bd66b5461339
bbx1209 <0weibo0@163.com>
2021-09-13 18:01:18 +0800

    collection divider

---
+++ Android/app/src/main/java/com/helloworld/sections/Collection/Activity/Model/CollectionItemDivider.java
@@ -0,0 +1,19 @@
+package com.helloworld.sections.Collection.Activity.Model;
+
+import android.graphics.Canvas;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CollectionItemDivider extends RecyclerView.ItemDecoration  {
+
+
+    public CollectionItemDivider() {
+        super();
+    }
+
+    @Override
+    public void onDraw(@NonNull Canvas c, @NonNull RecyclerView parent, @NonNull RecyclerView.State state) {
+        super.onDraw(c, parent, state);
+    }
+}
