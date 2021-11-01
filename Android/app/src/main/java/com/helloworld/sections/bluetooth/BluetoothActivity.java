dd9c0f7de97d2b55c39bcfdb9bfb7b941b33294a
bbx1209 <0weibo0@163.com>
2021-11-01 10:44:23 +0800

    bt activity

--- Android/app/src/main/java/com/helloworld/sections/bluetooth/BluetoothActivity.java
+++ Android/app/src/main/java/com/helloworld/sections/bluetooth/BluetoothActivity.java
@@ -3,6 +3,7 @@ package com.helloworld.sections.bluetooth;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.helloworld.R;
 
@@ -12,5 +13,13 @@ public class BluetoothActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bluetooth);
+
+
+    }
+    //开启蓝牙
+    public void openBT(View view) {
+    }
+    //搜索设备
+    public void findDevice(View view) {
     }
 }
